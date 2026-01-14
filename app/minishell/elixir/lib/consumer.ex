b6e3ccf3d300ac9680015ff8786cd9816c4d4e81
Everton <egeraldo@student.42sp.org.br>
2026-01-14 18:05:53 -0300

    feat: Atualiza configuração do consumidor de mensagens para usar variáveis de ambiente
    - Modifica o módulo Messaging.Consumer para utilizar variáveis de ambiente QUEUE_COMMAND e QUEUE_RESPONSE ao declarar filas e consumir mensagens.
    - Adiciona variáveis de ambiente no arquivo .env.development para configuração local.
    - Adiciona variáveis de ambiente no arquivo .env.production para configuração em produção.

--- app/minishell/elixir/lib/consumer.ex
+++ app/minishell/elixir/lib/consumer.ex
@@ -4,8 +4,11 @@ defmodule Messaging.Consumer do
     {:ok, connection} = AMQP.Connection.open(host: host)
     {:ok, channel} = AMQP.Channel.open(connection)
 
-    AMQP.Queue.declare(channel, "commands")
-    AMQP.Basic.consume(channel, "commands", nil, no_ack: true)
+    queue_command = System.get_env("QUEUE_COMMAND") || "commands"
+    queue_response = System.get_env("QUEUE_RESPONSE") || "responses"
+
+    AMQP.Queue.declare(channel, queue_command)
+    AMQP.Basic.consume(channel, queue_command, nil, no_ack: true)
 
     IO.puts(" [*] Waiting for messages. To exit press CTRL+C, CTRL+C")
 
@@ -28,11 +31,11 @@ defmodule Messaging.Consumer do
       {:ok, valid_msg} ->
         result = Messaging.Executer.execute(valid_msg)
         result = Messaging.Sanitizer.sinitize_response(result, valid_msg)
-        Messaging.Producer.publish("responses", result)
+        Messaging.Producer.publish(queue_response, result)
 
       {:error, reason} ->
         IO.puts(" [!] Sanitization error: #{reason}")
-        Messaging.Producer.publish("responses", reason)
+        Messaging.Producer.publish(queue_response, reason)
     end
   end
 end
