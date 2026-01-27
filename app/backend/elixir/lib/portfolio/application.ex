0ee74985315299ce708e5aed031863f1d34248f2
Everton <egeraldo@student.42sp.org.br>
2026-01-27 17:51:34 -0300

    feat: ajusta configuração da porta no módulo Application para usar variável de ambiente

--- app/backend/elixir/lib/portfolio/application.ex
+++ app/backend/elixir/lib/portfolio/application.ex
@@ -5,8 +5,10 @@ defmodule Portfolio.Application do
 
   @impl true
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4000")
+
     children = [
-      {Bandit, plug: Portfolio.Router, scheme: :http, port: 4000}
+      {Bandit, plug: Portfolio.Router, scheme: :http, port: port, ip: {0, 0, 0, 0}}
     ]
 
     opts = [strategy: :one_for_one, name: Portfolio.Supervisor]
