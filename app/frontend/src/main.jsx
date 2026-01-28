277b773f368ffbd98af69cbcca1928fa8b3497be
Everton <egeraldo@student.42sp.org.br>
2026-01-28 16:27:19 -0300

    feat: adiciona verificação de ambiente de produção para desativar logs no console

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -4,6 +4,14 @@ import './styles/tailwind.css'
 import './i18n'
 import App from './App.jsx'
 
+
+if (import.meta.env.PROD) {
+  console.log = () => { };
+  console.info = () => { };
+  console.debug = () => { };
+  console.warn = () => { };
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
