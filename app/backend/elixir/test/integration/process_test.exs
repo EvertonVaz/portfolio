c19d405d2187cfe5af38f29a505f3a7f5b997eea
Everton <egeraldo@student.42sp.org.br>
2026-06-25 16:36:05 -0300

    init portfolio v2

--- app/backend/elixir/test/integration/process_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule Portfolio.IntegrationTest do
-  use ExUnit.Case
-
-  test "full flow execution" do
-    # This is similar to unit test in this simple architecture,
-    # but could test more interacting parts if we had them.
-    assert Portfolio.Terminal.execute("whoami") =~ "root"
-  end
-end
